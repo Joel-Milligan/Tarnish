4405c8dbd09331d67a02170b9199c5e8bd0dab46
Joel Milligan <joelmilligan24@gmail.com>
2023-05-18 16:42:10 +0800

    Add more helpful panic message using the vga buffer

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("5 + 3 = {}", 5 + 3);
+    panic!("Manual panic");
     loop {}
 }
