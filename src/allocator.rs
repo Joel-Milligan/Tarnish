51062e5997ccf6dd5ec290da257ecbfed15f203c
Joel Milligan <joelmilligan24@gmail.com>
2023-05-22 15:38:48 +0800

    Stub allocator

---
+++ src/allocator.rs
@@ -0,0 +1,16 @@
+use core::{alloc::GlobalAlloc, ptr::null_mut};
+
+#[global_allocator]
+static ALLOCATOR: Dummy = Dummy;
+
+pub struct Dummy;
+
+unsafe impl GlobalAlloc for Dummy {
+    unsafe fn alloc(&self, _layout: core::alloc::Layout) -> *mut u8 {
+        null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: core::alloc::Layout) {
+        panic!("dealloc should never be called")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,12 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::memory::BootInfoFrameAllocator;
-use rust_os::{memory, println};
-use x86_64::structures::paging::Page;
-use x86_64::VirtAddr;
+use rust_os::println;
 
 entry_point!(kernel_main);
 
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     rust_os::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
