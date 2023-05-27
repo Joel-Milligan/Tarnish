2484a8349c105764129af61103698384a523e848
Joel Milligan <joelmilligan24@gmail.com>
2023-05-27 15:37:15 +0800

    Remove unused function

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -24,22 +24,6 @@ pub fn hlt_loop() -> ! {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
 pub fn init_logger(
     framebuffer: &'static mut [u8],
     info: FrameBufferInfo,
